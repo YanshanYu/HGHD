ccda8200619d717fc73c5cf4c70a47b0c20d8ea2
Yanshan Yu <1004521494@qq.com>
2021-03-01 17:47:46 +0800

    update

--- app/src/main/java/com/yu/zehnit/ui/rehabilitation/RehabilitationFragment.java
+++ app/src/main/java/com/yu/zehnit/ui/rehabilitation/RehabilitationFragment.java
@@ -1,4 +1,4 @@
-package com.yu.zehnit.ui.notifications;
+package com.yu.zehnit.ui.rehabilitation;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -14,17 +14,17 @@ import androidx.lifecycle.ViewModelProvider;
 
 import com.yu.zehnit.R;
 
-public class NotificationsFragment extends Fragment {
+public class RehabilitationFragment extends Fragment {
 
-    private NotificationsViewModel notificationsViewModel;
+    private RehabilitationViewModel rehabilitationViewModel;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        notificationsViewModel =
-                new ViewModelProvider(this).get(NotificationsViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_notifications, container, false);
-        final TextView textView = root.findViewById(R.id.text_notifications);
-        notificationsViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+        rehabilitationViewModel =
+                new ViewModelProvider(this).get(RehabilitationViewModel.class);
+        View root = inflater.inflate(R.layout.fragment_rehabilitation, container, false);
+        final TextView textView = root.findViewById(R.id.text_rehabilitation);
+        rehabilitationViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
                 textView.setText(s);
--- app/src/main/java/com/yu/zehnit/ui/rehabilitation/RehabilitationViewModel.java
+++ app/src/main/java/com/yu/zehnit/ui/rehabilitation/RehabilitationViewModel.java
@@ -1,16 +1,16 @@
-package com.yu.zehnit.ui.dashboard;
+package com.yu.zehnit.ui.rehabilitation;
 
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
-public class DashboardViewModel extends ViewModel {
+public class RehabilitationViewModel extends ViewModel {
 
     private MutableLiveData<String> mText;
 
-    public DashboardViewModel() {
+    public RehabilitationViewModel() {
         mText = new MutableLiveData<>();
-        mText.setValue("This is dashboard fragment");
+        mText.setValue("This is rehabilitation fragment");
     }
 
     public LiveData<String> getText() {
