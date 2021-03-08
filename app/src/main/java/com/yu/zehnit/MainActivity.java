f8d0a4bb411f5d0b1414b7346d48631e085bdeba
duxx_glb <1215532732@qq.com>
2021-03-08 17:22:32 +0800

    login --v3

--- app/src/main/java/com/yu/zehnit/MainActivity.java
+++ app/src/main/java/com/yu/zehnit/MainActivity.java
@@ -1,6 +1,9 @@
 package com.yu.zehnit;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
@@ -27,6 +30,7 @@ public class MainActivity extends BaseActivity {
         NavigationUI.setupWithNavController(navView, navController);
 
     }
+
 
     @Override
     public void onBackPressed() {
