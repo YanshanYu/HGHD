48d10f1669de92a76a1454d4d68cd196807c7cf6
Yanshan Yu <1004521494@qq.com>
2021-03-10 09:46:14 +0800

    update_yys

--- app/src/main/java/com/yu/zehnit/VideoPlayerActivity.java
+++ app/src/main/java/com/yu/zehnit/VideoPlayerActivity.java
@@ -11,7 +11,6 @@ import android.widget.VideoView;
 
 public class VideoPlayerActivity extends AppCompatActivity {
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
